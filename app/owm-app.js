b23b949f6bf8177d425ffa64e3516eb40192431d
sairah814 <sairah814@gmail.com>
2016-05-17 15:51:45 -0400

    basic routing

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,1 +1,38 @@
-angular.module('OWMApp', ['ngRoute']);
+angular.module('OWMApp', ['ngRoute'])
+    .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/cities/:city', {
+                templateUrl: 'city.html',
+                controller: 'CityCtrl',
+                resolve: {
+                    city: function (owmCities, $route, $location) {
+                        var city = $route.current.params.city;
+                        if (owmCities.indexOf(city) === -1) {
+                            $location.path('/error');
+                            return;
+                        }
+                        return city;
+                    }
+                }
+            })
+            .when('/error', {
+                template: '<p>Error - Page Not Found</p>'
+            });
+    }])
+    .controller('HomeCtrl', ['$scope', function ($scope) {
+        //empty for now
+    }])
+    .controller('CityCtrl', function ($scope, $routeParams, owmCities) {
+        //$scope.city = $routeParams.city;
+        var city = $routeParams.city;
+        if (owmCities.indexOf(city) === -1) {
+            //city not found
+            return;
+        }
+
+        $scope.city = city;
+    });
